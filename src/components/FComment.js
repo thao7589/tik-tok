50d1174b6184222b5fcd6195be12c9120fe45ced
Thao Le <thao.le@s3corp.com.vn>
2022-08-22 18:42:50 +0700

    add semantic ui

---
+++ src/components/FComment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FComment = () => {
+    return (
+        <div>
+            <div className="ui container comment">
+                <a href="" className="avatar">
+                    <img src="" alt="avatar" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default FComment;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Body from './Body';
 
 class Product extends Component {
     render() {
         return (
-            <Header/>
+            <div>
+            </div>    
         )
     }
 }

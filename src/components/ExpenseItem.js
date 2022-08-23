f2c0667023500304c8389b5f7f8edfa0542285a8
thao <leanhthao01@gmail.com>
2022-08-24 00:25:37 +0700

    add map

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+    console.log(props)
+    return(
+        <div className='expense-item'>
+            <div>{props.date}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;

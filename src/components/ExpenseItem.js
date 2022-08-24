ee9b6a56c019744f6c0b340f54086b209fa98c07
Thao Le <thao.le@s3corp.com.vn>
2022-08-24 19:51:41 +0700

    add Expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-    console.log(props)
-    return(
-        <div className='expense-item'>
-            <div>{props.date}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;

56e9241c4adbaf1360a5614cca233d8ed205cc3f
Thao Le <thao.le@s3corp.com.vn>
2022-09-07 19:37:24 +0700

    pass data throught components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
+  let expenses = [];
+  const addExpenseHandler = (expense) => {
+    console.log("in app.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
-      <Expenses />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }

d5d3d191ca890118870be0aecd1b31d9353d046a
Thao Le <thao.le@s3corp.com.vn>
2022-09-13 17:24:52 +0700

    get Filter works

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,27 +5,27 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  // const expenses = [
+  //   {
+  //     id: "e1",
+  //     title: "Toilet Paper",
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
+  //   },
+  //   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  //   {
+  //     id: "e3",
+  //     title: "Car Insurance",
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     id: "e4",
+  //     title: "New Desk (Wooden)",
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   },
+  // ];
 
   // const expenseItems = expenses.map( expense => {
   //   return (
@@ -40,6 +40,10 @@ const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear] = useState("2020");
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -50,15 +54,18 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-
-      {props.items.map((expense) => (
-        <ExpenseItem
-          id={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {filteredExpenses.length === 0 ? (
+        <p>No expenses found.</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
     </Card>
   );
 };

ce8748590b785d768ede72d3e599caed0133571d
Thao Le <thao.le@s3corp.com.vn>
2022-09-27 18:23:37 +0700

    69 in progress

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       return [expense, ...expenses];
     });
   };
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,7 +42,6 @@ const ExpenseForm = (props) => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
     props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
